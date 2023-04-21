a7aee00272522860441a109983c3bb3b660202d1
TrungHieu-Devoloper <hieudz045k@gmail.com>
2023-04-22 02:38:14 +0700

    update code

--- script.js
+++ script.js
@@ -127,3 +127,10 @@ function play(){
     }
     requestAnimationFrame(create_pipe);
 }
+
+
+function calcRate(r) {
+    const f = ~~r,//Tương tự Math.floor(r)
+    id = 'star' + f + (r % f ? 'half' : '')
+    id && (document.getElementById(id).checked = !0)
+   }
